80771ce159d20fe6a9874a1c3fbade24d13d8040
Sabrina Mesa <msabrina88@gmail.com>
2016-10-08 13:49:51 -0400

    pokeStore

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log('I work');
-
+//make array of all pokemons so I can loop and randomize.
 var srcArray = [
         'img/fishy.png',
         'img/Eevee.png',
@@ -16,41 +16,66 @@ var srcArray = [
         'img/Zubat.png'
          ];
 var idArray = ['fishy', 'eevee', 'caterpie'];
+var pokeBalls = 9;
+var pokeCounter = 0;
+var pokeBallInHand = true;
 
 $(document).ready(function () {
   randomOnLoad();
 
-
+// make pokemon show up when box they are in is clicked.
 var $findPokemon = $('.pokemon');
 function pokemonVisibility(event) {
-  console.log('show pokemon');
-  $(this).css({'opacity': '1'});
-}
+  if (pokeBallInHand) {
+    pokeBalls--;
+    console.log('show pokemon');
+    $(this).css({'opacity': '1'});
+    pokeBallInHand = false;
 
-$findPokemon.click(pokemonVisibility);
-
-function randomOnLoad() {
-var allBoxes = $('.pokemon');
-  for (var i = 0; i < srcArray.length; i++) {
-    var thisBox = allBoxes.eq(Math.floor(Math.random() * allBoxes.length));
-  while (thisBox.attr('src') != '') {
-      var thisBox = allBoxes.eq(Math.floor(Math.random() * allBoxes.length));
-    }
-    //thisBox.attr('id', idArray[i]);
-    thisBox.attr('src', srcArray[i]);
-    console.log(thisBox);
+    // if the div you clicked on has src (! empty)
+    // get src img and append it to your container
+  } else {
+    console.log('You dont have a pokeball stupid');
   }
 }
 
-event.preventDefault();
+$findPokemon.click(pokemonVisibility);
+// option a. decrease pokeball
+
+  function randomOnLoad() {
+  var allBoxes = $('.pokemon');
+    for (var i = 0; i < srcArray.length; i++) {
+      var thisBox = allBoxes.eq(Math.floor(Math.random() * allBoxes.length));
+    while (thisBox.attr('src') != '') {
+      var thisBox = allBoxes.eq(Math.floor(Math.random() * allBoxes.length));
+      }
+      //thisBox.attr('id', idArray[i]);
+      thisBox.attr('src', srcArray[i]);
+      console.log(thisBox);
+    }
+  }
+
+  var $pokeStore = $('.pokeStore');
+  console.log($pokeStore);
+  $($pokeStore).append(pokeBalls);
+
+  function getPoke(event) {
+    console.log("You got another PokeBall!!")
+    if (pokeBallInHand) {
+      console.log("You already got a PokeBall in your hand");
+    } else if (pokeBalls > 0) {
+      pokeBallInHand = true;
+    } else {
+      alert('No PokeBalls left! You lose!!')
+    }
+  };
+  $('.pokeStore').click(getPoke);
+
 });
-
-
-function drag(e) {
-    var pokeBalls =
-    e.dataTransfer.setData("text", ev.target.id);
-}
-
 //create function when mouse clicks on poke ball, cursor turns into pb and picks up 3 balls
+//append pokeBalls to pokeCounter
 //create function that releases a ball from cursor when box is clicked.
-//Once the clicks have been made cursor turns normal and you have to go get some more
+//Once the clicks have been made cursor turns normal and you have to go get more balls
+
+
+  //console.log('poke balls reloaded!');
