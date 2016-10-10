ddd3f297a1a9fda93327d756bbb9c3b9bd3ff421
Sabrina Mesa <msabrina88@gmail.com>
2016-10-09 23:01:41 -0400

    landing page

--- script.js
+++ script.js
@@ -20,29 +20,11 @@ var pokeBalls = 9;
 var pokeCounter = 0;
 var pokeBallInHand = true;
 
-$(document).ready(function () {
+
+$(document).ready(function() {
   randomOnLoad();
 
-// make pokemon show up when box they are in is clicked.
-var $findPokemon = $('.pokemon');
-function pokemonVisibility(event) {
-  if (pokeBallInHand) {
-    pokeBalls--;
-    console.log('show pokemon');
-    $(this).css({'opacity': '1'});
-    pokeBallInHand = false;
-
-    // if the div you clicked on has src (! empty)
-    // get src img and append it to your container
-  } else {
-    console.log('You dont have a pokeball stupid');
-  }
-}
-
-$findPokemon.click(pokemonVisibility);
-// option a. decrease pokeball
-
-  function randomOnLoad() {
+function randomOnLoad() {
   var allBoxes = $('.pokemon');
     for (var i = 0; i < srcArray.length; i++) {
       var thisBox = allBoxes.eq(Math.floor(Math.random() * allBoxes.length));
@@ -54,6 +36,32 @@ $findPokemon.click(pokemonVisibility);
       console.log(thisBox);
     }
   }
+
+
+// make pokemon show up when box they are in is clicked.
+var $findPokemon = $('.pokemon');
+var src = $('src');
+function pokemonVisibility(event) {
+  // debugger
+  if (pokeBallInHand) {
+    pokeBalls--;
+    console.log('show pokemon');
+    $(this).css({'opacity': '1'});
+    pokeBallInHand = false;
+  } else {
+    console.log('You dont have a pokeball stupid');
+  }
+  if (this.attributes.src.nodeValue !== "") {
+    alert('You found a Pokemon!! Yay!')
+    console.log('poke');
+  } else console.log('no poke');
+}
+
+$findPokemon.click(pokemonVisibility);
+
+ // if the div you clicked on has src (! empty)
+    // get src img and append it to your container
+
 
   var $pokeStore = $('.pokeStore');
   console.log($pokeStore);
@@ -78,4 +86,22 @@ $findPokemon.click(pokemonVisibility);
 //Once the clicks have been made cursor turns normal and you have to go get more balls
 
 
-  //console.log('poke balls reloaded!');
+
+Step 1
+let query = window.location.search.substring(1);
+let urlStringUser = query.split("&");
+
+
+for (let i = 0; i <urlStringUser.length; i++) {
+ let tempArr = [];
+ tempArr = urlStringUser[i].split("=");
+ let key = tempArr[0];
+ let value = tempArr[1];
+   if (i === 1) {
+     let uniqueUser = value;
+     let greeting = $('h3');
+     greeting.append(value);
+   }
+}
+
+
